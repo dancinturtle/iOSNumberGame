2b7cb63a135a5b24b63ea801b9c17248ed41fb03
dancinturtle <amy.giver@gmail.com>
2016-09-08 13:06:51 -0700

    Initial Commit

---
+++ numberGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  numberGame
+//
+//  Created by Amy Giver on 9/8/16.
+//  Copyright © 2016 Amy Giver Squid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
